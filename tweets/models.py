2db234db8d63df78717ec9c1574717933010083a
WTQ <707099242@qq.com>
2022-04-12 16:07:17 +0800

    creat tweet modle

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from utils.time_helpers import utc_now
 
-# Create your models here.
+class Tweet(models.Model):
+    # 帖子是谁发的
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, help_text='谁发了这个帖子')
+    content = models.CharField(max_length=255)
+
+    # auto_now_add 表示只在创建的时候更新
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def hours_to_now(self):
+        # datetime.now 不带时区信息，需要增加上 utc 的时区信息
+        # django的createdat会添加时区信息
+        return (utc_now() - self.created_at).seconds // 3600
+
+    def __str__(self):
+        # 这里是你执行 print(tweet instance) 的时候会显示的内容
+        return f'{self.created_at} {self.user}: {self.content}'
