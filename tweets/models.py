21d5746708973495d5d05d58eef3e5417e7cc70b
WTQ <707099242@qq.com>
2022-04-23 14:16:59 +0800

    16-likes-create-api

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from utils.time_helpers import utc_now
+from likes.models import Like
+from django.contrib.contenttypes.models import ContentType
 
 
 class Tweet(models.Model):
@@ -20,6 +22,13 @@ class Tweet(models.Model):
         # datetime.now 不带时区信息，需要增加上 utc 的时区信息
         # django的createdat会添加时区信息
         return (utc_now() - self.created_at).seconds // 3600
+
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
 
     def __str__(self):
         # 这里是你执行 print(tweet instance) 的时候会显示的内容
