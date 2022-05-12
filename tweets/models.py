e15f9f22b57920c86f27eb6d3f1203199eb56bbc
WTQ <707099242@qq.com>
2022-05-12 16:44:06 +0800

    1

--- tweets/models.py
+++ tweets/models.py
@@ -19,6 +19,11 @@ class Tweet(models.Model):
     # auto_now_add 表示只在创建的时候更新
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # 新增的 field 一定要设置 null=True，否则 default = 0 会遍历整个表单去设置
+    # 导致 Migration 过程非常慢，从而把整张表单锁死，从而正常用户无法创建新的 tweets
+    likes_count = models.IntegerField(default=0, null=True)
+    comments_count = models.IntegerField(default=0, null=True)
+
     class Meta:
         index_together = (('user', 'created_at'),)
         ordering = ('user', '-created_at')
